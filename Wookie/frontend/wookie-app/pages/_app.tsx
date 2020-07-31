b1fb85b6ce0dc31439236f90e8747c363f3875ef
Mathieu Delphin <delphi.mat@gmail.com>
2020-07-31 16:21:51 +0200

    We setup the skeleton : - sidebar - topbar, - input for the search bar.

--- Wookie/frontend/wookie-app/pages/_app.tsx
+++ Wookie/frontend/wookie-app/pages/_app.tsx
@@ -1,6 +1,7 @@
 import App from 'next/app'
 import Head from "next/head";
 import Navbar from "../components/navbar"
+import TopBar from "../components/topbar";
 
 class MovieApp extends App {
 
@@ -15,8 +16,44 @@ class MovieApp extends App {
 
     return (
         <div>
-          <Navbar/>
-          <Component {...pageProps} />
+            <Head>
+                <meta charSet="utf-8"/>
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+                <meta name="description" content="" />
+                <link rel="icon" type="image/png" href="/img/logo-icon.png" />
+                <title>Wookie - Movies</title>
+                <link rel="stylesheet" type="text/css" href="/vendor/slick/slick.min.css" />
+                <link rel="stylesheet" type="text/css" href="/vendor/slick/slick-theme.min.css" />
+                <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css" />
+                <link href="/css/style.min.css" rel="stylesheet"/>
+            </Head>
+            <div id="page-top">
+                <div id="wrapper">
+                    <Navbar/>
+                    <div id="content-wrapper" className="d-flex flex-column">
+                        <div id="content">
+                            <TopBar/>
+                            <div className="container-fluid">
+                                <Component {...pageProps} />
+                            </div>
+                        </div>
+                        <footer className="sticky-footer bg-white">
+                            <div className="container my-auto">
+                                <div className="copyright text-center my-auto">
+                                    <span>Wookie <i className="fas fa-heart fa-fw text-danger"></i> by
+                                        <a target="https://validitylabs.org/" href="#">validitylabs</a>
+                                    </span>
+                                </div>
+                            </div>
+                        </footer>
+                    </div>
+                </div>
+            </div>
+
+
+
+
         </div>
 
     )
