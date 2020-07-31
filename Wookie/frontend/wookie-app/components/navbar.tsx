da3f259e68ddd08d0bcd013dd76311cd763a8b6d
Mathieu Delphin <delphi.mat@gmail.com>
2020-07-31 16:03:08 +0200

    add the component SIDEBAR

---
+++ Wookie/frontend/wookie-app/components/navbar.tsx
@@ -0,0 +1,37 @@
+const NavBar = () => {
+    return (
+        <ul className="navbar-nav sidebar sidebar-dark accordion" id="accordionSidebar">
+            <a className="sidebar-brand d-flex align-items-center justify-content-center" href="#">
+                <div className="sidebar-brand-text mx-3">
+                    <img src="img/logo3.png" alt=""/>
+                </div>
+            </a>
+            <li className="nav-item active">
+                <a className="nav-link" href="#">
+                    <i className="fas fa-fw fa-film"></i>
+                    <span>Movies</span>
+                </a>
+            </li>
+            <hr className="sidebar-divider" />
+                <li className="nav-item">
+                    <a className="nav-link" href="#">
+                        <i className="fas fa-fw fa-fire"></i>
+                        <span> - Action </span>
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">
+                        <i className="fas fa-fw fa-fire"></i>
+                        <span> - Horror </span>
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">
+                        <i className="fas fa-fw fa-fire"></i>
+                        <span> - Fun </span>
+                    </a>
+                </li>
+        </ul>
+    )
+}
+export default NavBar
--- Wookie/frontend/wookie-app/pages/_app.tsx
+++ Wookie/frontend/wookie-app/pages/_app.tsx
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import App from 'next/app'
+import Head from "next/head";
+import Navbar from "../components/navbar"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+class MovieApp extends App {
+
+  static async getInitialProps(appContext) {
+    const appProps = await App.getInitialProps(appContext)
+
+    return { ...appProps }
+  }
+
+  render () {
+    const { Component, pageProps } = this.props
+
+    return (
+        <div>
+          <Navbar/>
+          <Component {...pageProps} />
+        </div>
+
+    )
+  }
+
 }
 
-export default MyApp
+export default MovieApp
