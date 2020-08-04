66aa35e9e8548310d9eb6d372f4883dd7f517514
Mathieu Delphin <delphi.mat@gmail.com>
2020-08-04 17:35:30 +0200

    we use the same movie.json from the script to push the data on algolia engine and the api in nest.js

--- Wookie/backend/wookie-api/algolia/pushDataSearchEngine.js
+++ Wookie/backend/wookie-api/algolia/pushDataSearchEngine.js
@@ -4,7 +4,7 @@ const algoliasearch = require('algoliasearch');
 const client = algoliasearch('OQGENTZBQK', '6e79ad3d80cfdc48bed421d00888064f');
 const index = client.initIndex('dev_wookie_movie');
 
-const contactsJSON = require('./resources/movies.json');
+const contactsJSON = require('../resources/movies.json');
 
 // remove old datas
 index.clearObjects()
